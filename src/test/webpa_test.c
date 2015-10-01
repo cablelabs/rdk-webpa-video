5f95d10ff1b673071c48663ef4a3eb5a90699585
ssachithanantham <Saravanan_Sachithanantham2@cable.comcast.com>
2015-10-01 18:25:30 +0530

    RDK-13996 : Test WAL implementation Reason for change: Test getValues and setValues Test Procedure: ./webpatest <get/set> <optional set of parameters> Risks: Less
    Change-Id: I9b322804ad8058d664885274ed2011d25edd6fe6
    Signed-off-by: ssachithanantham <Saravanan_Sachithanantham2@cable.comcast.com>

---
+++ src/test/webpa_test.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "wal.h"
+
+#define MAX_PARAMETER_LENGTH 512
+#define MAX_NUM_PARAMETERS 512
+#define MAX_VALUE_LENGTH 48
+
+int main(int argc,char *argv[])
+{
+msgBusInit("Webpa");
+char *getParamList[MAX_NUM_PARAMETERS];
+int i = 0;
+int j = 0;
+int paramCount = 1;
+
+if(argc<=1)
+{
+	printf("Usage: ./webpatest <set/get> <optional list of parameters>");
+	exit(0);
+}
+
+if(argc>2)
+{
+	paramCount = argc-2;
+	for(i=0;i<paramCount;i++)
+	{
+		getParamList[i] = (char *) malloc(MAX_PARAMETER_LENGTH);		
+		strncpy(getParamList[i],argv[i+2],MAX_PARAMETER_LENGTH-1);
+		getParamList[i][MAX_PARAMETER_LENGTH] = '\0';
+		printf("getParamList[%d] is %s\n",i,getParamList[i]);
+	}
+}
+else
+{
+	getParamList[0]="Device.WiFi.Radio.1.Status";
+}
+
+WAL_STATUS *ret = (WAL_STATUS *) malloc(sizeof(WAL_STATUS) * paramCount);
+
+
+//Test getValues
+if(!strcmp(argv[1],"get"))
+{
+int *count = (int *) malloc(sizeof(int) * paramCount);
+ParamVal ***parametervalArr = (ParamVal ***) malloc( sizeof(ParamVal **) * paramCount);
+getValues((const char **)getParamList, paramCount, parametervalArr, count, ret);
+for(i=0;i<paramCount;i++)
+{
+	if(ret[i] == WAL_SUCCESS)
+	{
+	for(j=0;j<count[i];j++)
+	{
+		//printf("getValues returned parmetername isvalue is %s datatype is %d\n",(parametervalArr[i][j])->value,(parametervalArr[i][j])->type);
+		printf("getValues returned parmetername is: %s",(parametervalArr[i][j])->name);
+		switch((parametervalArr[i][j])->type)
+		{
+			case WAL_STRING:
+				printf(" value is:%s\n",(parametervalArr[i][j])->value);
+				break;
+			case WAL_INT:
+			case WAL_UINT:
+			case WAL_BOOLEAN:
+			case WAL_LONG:
+			case WAL_ULONG:
+				printf("value is:%d\n",*((int *)(parametervalArr[i][j])->value));
+				break;
+			default:
+				printf("\n");
+				break;
+		}
+	}
+	}
+}
+}
+else if(!strcmp(argv[1],"set")) /*Test setvalues*/
+{
+    ParamVal *paramvalArr = (ParamVal *) malloc(sizeof(ParamVal) * paramCount);
+    for (i = 0; i < paramCount; i++)
+    {
+	paramvalArr[i].name = getParamList[i];
+	paramvalArr[i].value = (char *) malloc(sizeof(char) * MAX_VALUE_LENGTH);
+	strcpy(paramvalArr[i].value,"up"); 
+	paramvalArr[i].type = WAL_STRING;
+    }
+    setValues(paramvalArr,paramCount,0,ret);
+}
+else
+{
+	printf("Unsupported action\n");
+	printf("Usage: ./webpatest <set/get> <optional list of parameters>");
+}
+
+   /*TODO: Free up allocated resources*/
+}
