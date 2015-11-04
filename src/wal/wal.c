e252043ca9e4cac4867cb1dd8ce784e3e32bdd30
ssachithanantham <Saravanan_Sachithanantham2@cable.comcast.com>
2015-11-04 11:21:50 +0000

    RDK-14278:webpa notifications Reason for change:Changes in wal for webpa notifications Test Procedure:None Risks:Low
    Change-Id: I390523f3b9200de295b15b5b38a74f0b5d731144
    Signed-off-by: ssachithanantham <Saravanan_Sachithanantham2@cable.comcast.com>

--- src/wal/wal.c
+++ src/wal/wal.c
@@ -16,11 +16,13 @@ static int set_ParamValues_tr69hostIf (HOSTIF_MsgData_t param);
 static int SetParamInfo(ParamVal paramVal);
 static int getParamAttributes(const char *pParameterName, AttrVal ***attr, int *TotalParams);
 static int setParamAttributes(const char *pParameterName, const AttrVal *attArr);
+static void  _tr69Event_handler(const char *owner, IARM_Bus_tr69HostIfMgr_EventId_t eventId, void *data, size_t len);
 
 static void converttohostIfType(char *ParamDataType,HostIf_ParamType_t* pParamType);
 static void converttoWalType(HostIf_ParamType_t paramType,DATA_TYPE* walType);
 
 static int g_dbhandle = 0;
+void (*notifyCbFn)(ParamNotify*) = NULL;
 
 static void converttohostIfType(char *ParamDataType,HostIf_ParamType_t* pParamType)
 {
@@ -119,6 +121,14 @@ WAL_STATUS msgBusInit(const char *name)
 		return WAL_FAILURE;
 	}
 
+	/* Register for IARM Events */
+	IARM_Bus_RegisterEventHandler(IARM_BUS_TR69HOSTIFMGR_NAME, IARM_BUS_TR69HOSTIFMGR_EVENT_ADD, _tr69Event_handler);
+	IARM_Bus_RegisterEventHandler(IARM_BUS_TR69HOSTIFMGR_NAME, IARM_BUS_TR69HOSTIFMGR_EVENT_REMOVE, _tr69Event_handler);
+	IARM_Bus_RegisterEventHandler(IARM_BUS_TR69HOSTIFMGR_NAME, IARM_BUS_TR69HOSTIFMGR_EVENT_VALUECHANGED, _tr69Event_handler);
+	/* Register call for notify events */
+	IARM_Bus_RegisterEvent(1);
+
+
 	// Load Document model
 	dbRet = loaddb("/etc/data-model.xml",(void *)&g_dbhandle);
 
@@ -129,6 +139,66 @@ WAL_STATUS msgBusInit(const char *name)
 	}
 }
 
+static void  _tr69Event_handler(const char *owner, IARM_Bus_tr69HostIfMgr_EventId_t eventId, void *data, size_t len)
+{
+	IARM_Bus_tr69HostIfMgr_EventData_t *tr69EventData = (IARM_Bus_tr69HostIfMgr_EventData_t *)data;
+	ParamNotify *paramNotify = (ParamNotify *) malloc(sizeof(ParamNotify));
+	memset(paramNotify,0,sizeof(ParamNotify));
+
+	if (0 == strcmp(owner, IARM_BUS_TR69HOSTIFMGR_NAME))
+	{
+		switch (eventId)
+		{
+		case IARM_BUS_TR69HOSTIFMGR_EVENT_ADD:
+			if(tr69EventData->paramName)
+			{
+				paramNotify->paramName = tr69EventData->paramName;
+			}
+			if(tr69EventData->paramValue)
+			{
+				paramNotify->newValue = tr69EventData->paramValue;
+			}
+			//paramNotify->oldValue= val->oldValue;
+			converttoWalType(tr69EventData->paramtype,&(paramNotify->type));
+			break;
+		case IARM_BUS_TR69HOSTIFMGR_EVENT_REMOVE:
+			if(tr69EventData->paramName)
+			{
+				paramNotify->paramName = tr69EventData->paramName;
+			}
+			if(tr69EventData->paramValue)
+			{
+				paramNotify->newValue = tr69EventData->paramValue;
+			}
+			//paramNotify->oldValue= val->oldValue;
+			converttoWalType(tr69EventData->paramtype,&(paramNotify->type));
+			break;
+		case IARM_BUS_TR69HOSTIFMGR_EVENT_VALUECHANGED:
+			if(tr69EventData->paramName)
+			{
+				paramNotify->paramName = tr69EventData->paramName;
+			}
+			if(tr69EventData->paramValue)
+			{
+				paramNotify->newValue = tr69EventData->paramValue;
+			}
+			converttoWalType(tr69EventData->paramtype,&(paramNotify->type));
+//    	paramNotify->changeSource = mapWriteID(val->writeID);
+			break;
+		default:
+			break;
+		}
+	}
+
+
+	RDK_LOG(RDK_LOG_INFO,LOG_MOD_WEBPA,"Notification Event from stack: Parameter Name: %s, Old Value: %s, New Value: %s, Data Type: %d, Write ID: %d\n", paramNotify->paramName, paramNotify->oldValue, paramNotify->newValue, paramNotify->type, paramNotify->changeSource);
+
+	if(notifyCbFn != NULL)
+	{
+		(*notifyCbFn)(paramNotify);
+	}
+
+}
 /**
  * @brief Registers the notification callback function.
  *
@@ -137,7 +207,8 @@ WAL_STATUS msgBusInit(const char *name)
  */
 WAL_STATUS RegisterNotifyCB(notifyCB cb)
 {
-	//TODO::
+	notifyCbFn = cb;
+	return WAL_SUCCESS;
 }
 
 /**
@@ -158,7 +229,7 @@ static int get_ParamValues_tr69hostIf(HOSTIF_MsgData_t *ptrParam)
 	}
 	else
 	{
-		 RDK_LOG(RDK_LOG_DEBUG,LOG_MOD_WEBPA,"[%s:%s:%d] The value for param: %s is %s paramLen : %d\n", __FILE__, __FUNCTION__, __LINE__, ptrParam->paramName,ptrParam->paramValue, ptrParam->paramLen);
+		RDK_LOG(RDK_LOG_DEBUG,LOG_MOD_WEBPA,"[%s:%s:%d] The value for param: %s is %s paramLen : %d\n", __FILE__, __FUNCTION__, __LINE__, ptrParam->paramName,ptrParam->paramValue, ptrParam->paramLen);
 	}
 
 	return WAL_SUCCESS;
@@ -236,7 +307,7 @@ static int GetParamInfo(const char *pParameterName, ParamVal ***parametervalArr,
 			parametervalArr[0][0]->name = NULL;
 			parametervalArr[0][0]->value = NULL;
 			parametervalArr[0][0]->type = 0;
-			
+
 			if(isParameterValid((void *)g_dbhandle,pParameterName,dataType))
 			{
 				*TotalParams = 1;
@@ -328,21 +399,21 @@ static int SetParamInfo(ParamVal paramVal)
 
 	char *pdataType = malloc(sizeof(char) * MAX_DATATYPE_LENGTH);
 	if(isParameterValid((void *)g_dbhandle,paramVal.name,pdataType))
-        {	
+	{
 		DATA_TYPE walType;
 		converttohostIfType(pdataType,&(Param.paramtype));
 		// Convert Param.paramtype to ParamVal.type
 		converttoWalType(Param.paramtype,&walType);
-		
+
 		if(walType != paramVal.type)
 		{
 			RDK_LOG(RDK_LOG_ERROR,LOG_MOD_WEBPA," Invalid Parameter type for %s\n",paramVal.name);
 			return WAL_ERR_INVALID_PARAMETER_TYPE;
 		}
-		
+
 		strncpy(Param.paramName, paramVal.name,strlen(paramVal.name));
 		strncpy(Param.paramValue, paramVal.value,strlen(paramVal.value));
-	
+
 		ret = set_ParamValues_tr69hostIf(Param);
 		RDK_LOG(RDK_LOG_DEBUG,LOG_MOD_WEBPA,"set_ParamValues_tr69hostIf %d\n",ret);
 	}
